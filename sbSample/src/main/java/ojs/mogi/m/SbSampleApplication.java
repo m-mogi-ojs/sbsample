1302f9f8f86d268a69af929a508c69f10a936830
茂木優樹 <mogi@mogi.local>
2018-08-18 15:52:57 +0900

    initial commit

---
+++ sbSample/src/main/java/ojs/mogi/m/SbSampleApplication.java
@@ -0,0 +1,12 @@
+package ojs.mogi.m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSampleApplication.class, args);
+	}
+}
